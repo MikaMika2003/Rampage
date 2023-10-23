31592a99842424867a50909bd2f0fcb5ebc1e1a1
MikaMika2003 <catgirl1431@gmail.com>
2023-10-23 15:59:40 -0500

    Leaderboard Tingzzzz

--- CS360 Project/Assets/Scripts/PlayFabManager.cs
+++ CS360 Project/Assets/Scripts/PlayFabManager.cs
@@ -5,8 +5,9 @@ using PlayFab.ClientModels;
 using PlayFab;
 using System;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayFabManager : MonoBehaviour
 {
     [Header("UI")]
     public Text messageText;
@@ -56,4 +57,36 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
 
+    public void SendLeaderboard(int score) {
+        var request = new UpdatePlayerStatisticsRequest {
+            Statistics = new List<StatisticUpdate> {
+                new StatisticUpdate {
+                    StatisticName = "GainedPoints",
+                    Value = score
+                }
+            }
+        };
+        PlayFabClientAPI.UpdatePlayerStatistics(request, OnLeaderboardUpdate, OnError);
+    }
+
+    void OnLeaderboardUpdate(UpdatePlayerStatisticsResult result)
+    {
+        Debug.Log("Successful leaderboard sent!");
+    }
+
+    public void GetLeaderboard() {
+        var request = new GetLeaderboardRequest {
+            StatisticName = "GainedPoints",
+            StartPosition = 0,
+            MaxResultsCount = 10
+        };
+        PlayFabClientAPI.GetLeaderboard(request, OnLeaderboardGet, OnError);
+    }
+
+    void OnLeaderboardGet(GetLeaderboardResult result)
+    {
+        foreach (var item in result.Leaderboard) {
+            Debug.Log(item.Position + " " + item.PlayFabId + " " + item.StatValue);
+        }
+    }
 }
