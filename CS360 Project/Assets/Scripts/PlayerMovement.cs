bb3b956b164c4f74cc100a190929d29e8de20e76
ajbeas2015 <130802986+ajbeas2015@users.noreply.github.com>
2023-10-26 16:43:57 -0500

    Player Movement script and Sprite Render

--- CS360 Project/Assets/Scripts/PlayerMovement.cs
+++ CS360 Project/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float horizontal;
+    private float speed = 8f;
+    //private float jumpForce = 10f;
+    private bool isFacingLeft = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;  
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        if (isFacingLeft && horizontal < 0f || !isFacingLeft && horizontal > 0f)
+        {
+            isFacingLeft = !isFacingLeft;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= 1f;
+            transform.localScale = localScale;
+        }
     }
 }
