ceb950f81a1dd95b1abdb16f159c7e7419aa21d0
skabettibread <andrea.florence180@topper.wku.edu>
2023-11-26 10:49:01 -0600

    MainMenu Function
    When scene if running, pressing F opens the actual game. Need to get this hooked up to the login

---
+++ CS360 Project/Assets/Scripts/OpenGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class OpenGame : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the "F" key is pressed
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            // Load your target scene
+            SceneManager.LoadScene("PlayingScene");
+        }
+    }
+}
